1ef1db7a3117e272f070b2b21f60678e0f115276
titoyanac <titoyanac@gmail.com>
2020-08-28 00:42:51 -0500

    by the way

--- src/java/Servlet/ServletPrueba.java
+++ src/java/Servlet/ServletPrueba.java
@@ -39,6 +39,7 @@ public class ServletPrueba extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServletPrueba at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ServletPrueba at " + request.getAttribute("nombre") + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
